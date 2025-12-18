4c3f89c5b8ae98a4094e0592e89841fc1503cc73
Afraj Charaniya <afrajcharniya264@gmail.com>
2025-12-19 00:55:36 +0530

    Delete app/auth directory

--- app/auth/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MaxWidthWrapper from "@/components/global/max-width-wrapper";
-
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-    return <div className="min-h-screen flex flex-col justify-center bg-background">
-        <MaxWidthWrapper className="justify-center flex">
-            {children}
-        </MaxWidthWrapper>
-    </div>;
-}
